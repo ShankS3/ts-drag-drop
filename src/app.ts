d84935244d8dcc6ef5a64acc421e9488d7be986d
ShankS3 <shashank.surwase@gmail.com>
2020-10-16 23:50:08 +0530

    Added logic to validate and handle input

--- src/app.ts
+++ src/app.ts
@@ -15,6 +15,48 @@ function autobind(
   return adjDescriptior;
 }
 
+interface Validatable {
+  value: string | number;
+  required?: boolean;
+  minLength?: number;
+  maxLength?: number;
+  min?: number;
+  max?: number;
+}
+
+function validate(validatableInput: Validatable) {
+  let isValid = true;
+  if(validatableInput.required) {
+    isValid = isValid && validatableInput.value.toString().trim().length !== 0;
+  }
+  if(
+    validatableInput.minLength != null && 
+    typeof validatableInput.value === 'string'
+  ) {
+    isValid = isValid && validatableInput.value.length > validatableInput.minLength;
+  }
+  if(
+    validatableInput.maxLength != null && 
+    typeof validatableInput.value === 'string'
+  ) {
+    isValid = isValid && validatableInput.value.length < validatableInput.maxLength;
+  }
+  if(
+    validatableInput.min != null && 
+    typeof validatableInput.value === 'number'
+  ) {
+    isValid = isValid && validatableInput.value > validatableInput.min;
+  }
+  if(
+    validatableInput.max != null && 
+    typeof validatableInput.value === 'number'
+  ) {
+    isValid = isValid && validatableInput.value < validatableInput.max
+  }
+
+  return isValid;
+}
+
 class Input {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
@@ -39,10 +81,55 @@ class Input {
     this.attach();
   }
 
+  private fetchUserInput(): [string, string, number] | void {
+    const title = this.titleInputElement.value;
+    const description = this.descriptionInputElement.value;
+    const people = this.peopleInputElement.value;
+
+    const titleValidatable: Validatable = {
+      value: title,
+      required: true
+    }
+
+    const descValidatable: Validatable = {
+      value: description,
+      required: true,
+      minLength: 5
+    }
+
+    const peopleValidatable: Validatable = {
+      value: people,
+      required: true,
+      min: 1
+    }
+
+    if(
+      !validate(titleValidatable) || 
+      !validate(descValidatable) || 
+      !validate(peopleValidatable)
+    ) {
+      alert('Invalid input, please try again!');
+      return;
+    } else {
+      return [title, description, +people]
+    }
+  }
+
+  private clearInputs() {
+    this.titleInputElement.value = '';
+    this.descriptionInputElement.value = '';
+    this.peopleInputElement.value = '';
+  }
+
   @autobind
   private submitHandler(event: Event){
     event.preventDefault();
-    console.log(this.titleInputElement.value);
+    const userInput = this.fetchUserInput();
+    if(Array.isArray(userInput)) {
+      const [title, desc, people ] = userInput;
+      console.log(title, desc, people);
+      this.clearInputs();
+    }
   }
 
   private configure() {
